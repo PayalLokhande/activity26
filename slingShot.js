ca0cfe835ec92cbcc0fe2e59d59ecf0845ad74b9
PayalLokhande <77966576+PayalLokhande@users.noreply.github.com>
2021-03-01 16:24:02 +0530

    Add files via upload

---
+++ slingShot.js
@@ -0,0 +1,15 @@
+class slingShot{
+    constructor(bodyA,bodyB){
+        var options={
+        bodyA:bodyA,
+        bodyB:bodyB,
+       stiffness:0.04,
+        length:10 }
+        this.slingShot=Constraint.create(options)
+        World.add(world,this.slingShot)
+    }
+    display(){
+        strokeWeight(4)
+        line(this.slingShot.bodyA.position.x,this.slingShot.bodyA.position.y,this.slingShot.bodyB.position.x,this.slingShot.bodyB.position.y)
+    }
+}
